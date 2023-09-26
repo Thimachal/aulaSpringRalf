54de76264aa80a046c7c9b48b586e5de01dbf4aa
Thimachal <thimachal@gmail.com>
2023-09-26 16:49:00 -0300

    first commit

--- src/main/java/br/com/thimaproject/thimaproject/model/Person.java
+++ src/main/java/br/com/thimaproject/thimaproject/model/Person.java
@@ -1,6 +1,14 @@
 package br.com.thimaproject.thimaproject.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private Integer age;
