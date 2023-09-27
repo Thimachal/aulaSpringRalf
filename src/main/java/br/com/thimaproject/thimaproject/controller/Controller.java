eeed98c560cd97bc597f63f26a22db9e2ee15ed9
Thimachal <thimachal@gmail.com>
2023-09-27 18:53:05 -0300

    id

--- src/main/java/br/com/thimaproject/thimaproject/controller/Controller.java
+++ src/main/java/br/com/thimaproject/thimaproject/controller/Controller.java
@@ -5,26 +5,30 @@ import br.com.thimaproject.thimaproject.repository.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class Controller {
     @Autowired
     private Repository action;
 
-    @PostMapping("/api")
+    //pega do banco o que é passado no corpo da requisição
+    @PostMapping("/persons")
     public Person register(@RequestBody Person obj){
         return action.save(obj);
     }
 
-    @GetMapping
-    public String mensseger(){
-       int num = 1+4;
-        return String.valueOf(num);
+    //faz a postagem de todo conteúdo requisitado no banco
+    @GetMapping("/persons")
+    public List<Person> selecionar(){
+        return action.findAll();
     }
 
-    @GetMapping("/{name}")
-    public String menssegerUrl(@PathVariable String name){
-        String phrase = "Eu sou programador Java, meu nome é: ";
-        return phrase + "Thima";
+
+    //pega o que do banco é passado via link
+    @GetMapping("/persons/{id}")
+    public Person id(@PathVariable Long id){
+        return action.findByCod(id);
     }
 
 }
--- src/main/java/br/com/thimaproject/thimaproject/repository/Repository.java
+++ src/main/java/br/com/thimaproject/thimaproject/repository/Repository.java
@@ -3,5 +3,19 @@ package br.com.thimaproject.thimaproject.repository;
 import br.com.thimaproject.thimaproject.model.Person;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface Repository extends CrudRepository<Person, Long> {
+
+    List<Person> findAll();
+
+
+    Person findByCod(Long id);
+    //Esta opção é da extensão Crudrepository
+    //Optional<Person> findById(Long condigo);
+
+
+    //se for retornar muitos registros por por id por exemplo, usa o abaixo:
+    //List<Pessoa> findById(Long id)
 }
