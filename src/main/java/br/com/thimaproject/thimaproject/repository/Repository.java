12f84d9d3f745ae52b4f24601a99c7c821bc52c3
Thimachal <thimachal@gmail.com>
2023-09-27 16:26:43 -0300

    repository

--- src/main/java/br/com/thimaproject/thimaproject/repository/Repository.java
+++ src/main/java/br/com/thimaproject/thimaproject/repository/Repository.java
@@ -1,4 +1,7 @@
 package br.com.thimaproject.thimaproject.repository;
 
-public interface Repository {
+import br.com.thimaproject.thimaproject.model.Person;
+import org.springframework.data.repository.CrudRepository;
+
+public interface Repository extends CrudRepository<Person, Long> {
 }
