3aad88eab678a644e72b4e0aeb28893b3bcf84e3
Thimachal <thimachal@gmail.com>
2023-10-03 17:08:57 -0300

    service

--- src/main/java/br/com/thimaproject/thimaproject/model/Messenger.java
+++ src/main/java/br/com/thimaproject/thimaproject/model/Messenger.java
@@ -4,4 +4,13 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Messenger {
+    private String menssenger;
+
+    public String getMenssenger() {
+        return menssenger;
+    }
+
+    public void setMenssenger(String menssenger) {
+        this.menssenger = menssenger;
+    }
 }
